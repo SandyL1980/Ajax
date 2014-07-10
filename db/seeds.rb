70e90309b7d68b5019bfa891b2a5056f86d56dec
Sandy Ludosky <sandyludosky@Sandys-MacBook-Pro.local>
2014-07-09 21:23:41 -0400

    Initial Commit - added gem'faker' - generated post scaffold - added search form_tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+100.times do 
+	Post.create(
+		author: Faker::Name.name,
+		title: Faker::Lorem.sentence,
+		content: Faker::Lorem.paragraph
+	)	
+end
